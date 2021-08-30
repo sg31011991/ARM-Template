1f5bb472692030eeb1c1936e847ffe3ebc04c0b9
Srimanta Ghosh <aznantu1991@outlook.com>
2021-08-30 22:12:04 +0530

    ARM

---
+++ ARM Template/ResourceGroup.ps1
@@ -0,0 +1,11 @@
+﻿Connect-AzAccount -Tenant 9172f0b8-2483-47b3-b435-0bd9b1b0dd45
+Select-AzSubscription -Subscription 533d386a-5ae6-4aa2-a60b-9d586b866f82
+$RGN = 'Demo-RG-ARM'
+$tags = @{"USe Case"="ARM Test"; "Owner"="Srimanta Ghosh"}
+$NRG = New-AzResourceGroup -Name $RGN -Location 'East US' -Tag $tags
+$RG = Get-AzResourceGroup -Name $RGN
+New-AzResourceGroupDeployment -Name deploy1 -ResourceGroupName $RG.ResourceGroupName -TemplateFile 'C:\ARM Template\102-Prod-Vnet.json'
+#New-AzResourceGroupDeployment -Name deployparam1 -ResourceGroupName $RG.ResourceGroupName -TemplateParameterFile 'C:\ARM Template\10-kv_deploymentScript.params.json' -TemplateFile 'C:\ARM Template\10-kv_deploymentScript.json'
+
+Read-Host 'delete RG ? press Enter to delete' 
+$RG | Remove-AzResourceGroup
---
+++ ARM Template/keyvault.ps1
@@ -0,0 +1,7 @@
+﻿$RG = Get-AzResourceGroup -Name $RGN
+$keyvault = New-AzKeyVault -Name "armkeyvaultaksdemo" -ResourceGroupName $RG.ResourceGroupName -Location "East US"
+$GKV= Get-AzKeyVault -ResourceGroupName $RG.ResourceGroupName
+Set-AzKeyVaultAccessPolicy -VaultName $GKV.VaultName -UserPrincipalName "nantu1991@aznantu1991outlook.onmicrosoft.com" -PermissionsToSecrets get,set,delete
+$secretvalue = ConvertTo-SecureString "Password@123" -AsPlainText -Force
+$secret = Set-AzKeyVaultSecret -VaultName $GKV.VaultName -Name "AzureVMPassword" -SecretValue $secretvalue
+$secret = Get-AzKeyVaultSecret -VaultName $GKV.VaultName -Name "AzureVMPassword" -AsPlainText
